93b83a6bde7b7b6d31aea67906e67d6455618119
Bhargava-Ram-Thunga <98541078+Bhargava-Ram-Thunga@users.noreply.github.com>
2026-07-05 13:18:51 +0530

    Remove UI completely

--- ui/src/app/autoloop/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getSession } from '@/lib/auth'
-import { redirect } from 'next/navigation'
-import AutoloopClient from './AutoloopClient'
-
-export default async function AutoloopPage() {
-  const session = await getSession()
-  if (!session) redirect('/login')
-  return <AutoloopClient />
-}
--- ui/src/app/feed/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getSession } from '@/lib/auth'
-import { redirect } from 'next/navigation'
-import FeedClient from './FeedClient'
-
-export default async function FeedPage() {
-  const session = await getSession()
-  if (!session) redirect('/login')
-  return <FeedClient />
-}
